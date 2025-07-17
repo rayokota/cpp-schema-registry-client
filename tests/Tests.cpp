f360bdc6e476c23d056409a5475cb054be2a22e5
Robert Yokota <rayokota@gmail.com>
2025-07-17 12:53:03 -0700

    Rename tests

--- tests/Tests.cpp
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-#include "srclient/rest/ClientConfiguration.h"
-
-TEST(MyLibraryTest, AddFunction) {
-    srclient::rest::ClientConfiguration config({"http://localhost:8080"});
-    EXPECT_EQ(config.getBaseUrls()[0], "http://localhost:8080");
-}
