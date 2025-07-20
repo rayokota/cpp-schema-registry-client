c5544d20fc0d5c121e3c0ef3c652297d11619c57
Robert Yokota <rayokota@gmail.com>
2025-07-20 00:05:15 -0700

    Clean up base64

--- src/serdes/json/JsonTypes.cpp
+++ src/serdes/json/JsonTypes.cpp
@@ -1,58 +1,18 @@
 #include "srclient/serdes/json/JsonTypes.h"
+#include "absl/strings/escaping.h"
 #include <algorithm>
 
 namespace srclient::serdes::json {
 
-// Base64 encoding/decoding utilities for JSON
+// Base64 decoding utilities for JSON using absl
 namespace {
-    const std::string base64_chars = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz"
-        "0123456789+/";
-
-    bool is_base64(unsigned char c) {
-        return (isalnum(c) || (c == '+') || (c == '/'));
-    }
-
     std::vector<uint8_t> base64_decode(const std::string& encoded_string) {
-        size_t in_len = encoded_string.size();
-        int i = 0;
-        int j = 0;
-        int in = 0;
-        unsigned char char_array_4[4], char_array_3[3];
-        std::vector<uint8_t> ret;
-        
-        while (in_len-- && (encoded_string[in] != '=') && is_base64(encoded_string[in])) {
-            char_array_4[i++] = encoded_string[in]; in++;
-            if (i == 4) {
-                for (i = 0; i < 4; i++)
-                    char_array_4[i] = base64_chars.find(char_array_4[i]);
-                    
-                char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-                char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-                char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-                
-                for (i = 0; (i < 3); i++)
-                    ret.push_back(char_array_3[i]);
-                i = 0;
-            }
+        std::string decoded;
+        if (!absl::Base64Unescape(encoded_string, &decoded)) {
+            // Return empty vector on decode failure
+            return std::vector<uint8_t>();
         }
-        
-        if (i) {
-            for (j = i; j < 4; j++)
-                char_array_4[j] = 0;
-                
-            for (j = 0; j < 4; j++)
-                char_array_4[j] = base64_chars.find(char_array_4[j]);
-                
-            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-            
-            for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
-        }
-        
-        return ret;
+        return std::vector<uint8_t>(decoded.begin(), decoded.end());
     }
 }
 
--- src/serdes/protobuf/ProtobufUtils.cpp
+++ src/serdes/protobuf/ProtobufUtils.cpp
@@ -1,4 +1,5 @@
 #include "srclient/serdes/protobuf/ProtobufUtils.h"
+#include "absl/strings/escaping.h"
 #include <google/protobuf/util/json_util.h>
 #include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/io/coded_stream.h>
@@ -6,96 +7,20 @@
 
 namespace srclient::serdes::protobuf::utils {
 
-// Base64 encoding/decoding utilities (copied from Dek.cpp)
+// Base64 encoding/decoding utilities using absl
 namespace {
-    const std::string base64_chars = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz"
-        "0123456789+/";
-
-    bool is_base64(unsigned char c) {
-        return (isalnum(c) || (c == '+') || (c == '/'));
-    }
-
     std::string base64_encode(const std::vector<uint8_t>& bytes) {
-        std::string ret;
-        int i = 0;
-        int j = 0;
-        unsigned char char_array_3[3];
-        unsigned char char_array_4[4];
-        
-        for (size_t idx = 0; idx < bytes.size(); idx++) {
-            char_array_3[i++] = bytes[idx];
-            if (i == 3) {
-                char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-                char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-                char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-                char_array_4[3] = char_array_3[2] & 0x3f;
-                
-                for (i = 0; (i < 4); i++)
-                    ret += base64_chars[char_array_4[i]];
-                i = 0;
-            }
-        }
-        
-        if (i) {
-            for (j = i; j < 3; j++)
-                char_array_3[j] = '\0';
-            
-            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-            char_array_4[3] = char_array_3[2] & 0x3f;
-            
-            for (j = 0; (j < i + 1); j++)
-                ret += base64_chars[char_array_4[j]];
-            
-            while ((i++ < 3))
-                ret += '=';
-        }
-        
-        return ret;
+        std::string input(reinterpret_cast<const char*>(bytes.data()), bytes.size());
+        return absl::Base64Escape(input);
     }
 
     std::vector<uint8_t> base64_decode(const std::string& encoded_string) {
-        int in_len = encoded_string.size();
-        int i = 0;
-        int j = 0;
-        int in = 0;
-        unsigned char char_array_4[4], char_array_3[3];
-        std::vector<uint8_t> ret;
-
-        while (in_len-- && (encoded_string[in] != '=') && is_base64(encoded_string[in])) {
-            char_array_4[i++] = encoded_string[in]; in++;
-            if (i == 4) {
-                for (i = 0; i < 4; i++)
-                    char_array_4[i] = base64_chars.find(char_array_4[i]);
-
-                char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-                char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-                char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-                for (i = 0; (i < 3); i++)
-                    ret.push_back(char_array_3[i]);
-                i = 0;
-            }
+        std::string decoded;
+        if (!absl::Base64Unescape(encoded_string, &decoded)) {
+            // Return empty vector on decode failure
+            return std::vector<uint8_t>();
         }
-
-        if (i) {
-            for (j = i; j < 4; j++)
-                char_array_4[j] = 0;
-
-            for (j = 0; j < 4; j++)
-                char_array_4[j] = base64_chars.find(char_array_4[j]);
-
-            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-            for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
-        }
-
-        return ret;
+        return std::vector<uint8_t>(decoded.begin(), decoded.end());
     }
 }
 
