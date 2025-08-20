61beb15be597c4bd816a24f1950258f7a0207787
Robert Yokota <rayokota@gmail.com>
2025-08-20 12:51:34 -0700

    Fix restex in dek

--- src/rest/DekRegistryClient.cpp
+++ src/rest/DekRegistryClient.cpp
@@ -131,7 +131,7 @@ std::string DekRegistryClient::sendHttpRequest(
     if (result->status >= 400) {
         std::string errorMsg = "HTTP Error " + std::to_string(result->status) +
                                ": " + result->body;
-        throw schemaregistry::rest::RestException(errorMsg);
+        throw schemaregistry::rest::RestException(errorMsg, result->status);
     }
 
     return result->body;
